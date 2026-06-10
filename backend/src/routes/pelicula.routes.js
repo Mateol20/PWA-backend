486d1488d7e0627aa17f1a27158c9e8013667300
BonorinoIgnacio <bonorinon@gmail.com>
2026-06-10 13:12:39 -0300

    funcion favoritos desde bk

--- backend/src/routes/pelicula.routes.js
+++ backend/src/routes/pelicula.routes.js
@@ -5,12 +5,13 @@ import { toggleFavorito, getFavoritos } from "../controllers/pelicula.controller
 const router = Router();
 
 router.get("/", peliculaController.getAll);
-router.get("/:id", peliculaController.getById);
 router.post("/", peliculaController.create);
+router.get("/favoritas", peliculaController.getFavoritos)
+
+router.get("/:id", peliculaController.getById);
 router.put("/:id", peliculaController.update);
 router.delete("/:id", peliculaController.remove);
 
-router.get("/favoritas", peliculaController.getFavoritos)
 router.patch("/:id/favorito", peliculaController.toggleFavorito);
 
 export default router;
